72d1c8b42333afe0f82b9806f04e0355a5c9c635
Phil Whiles <phil.whiles@gmail.com>
2019-10-13 10:17:31 +0100

    CR-560 pom tidy

---
+++ src/main/java/uk/gov/ons/ctp/integration/eqlaunch/service/EqLaunchService.java
@@ -0,0 +1,15 @@
+package uk.gov.ons.ctp.integration.eqlaunch.service;
+
+import uk.gov.ons.ctp.common.model.Channel;
+import uk.gov.ons.ctp.common.model.Language;
+import uk.gov.ons.ctp.integration.caseapiclient.caseservice.model.CaseContainerDTO;
+
+public interface EqLaunchService {
+
+  String getEqLaunchJwe(
+      Language language,
+      Channel channel,
+      CaseContainerDTO caseContainer,
+      String userId,
+      String accountServiceUrl);
+}
