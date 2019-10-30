475b63dcb6ce5e51e0edbfe3a875e8872d28b996
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-10-30 12:22:07 +0000

    CR-560 Switched payload from String to Map

--- src/test/java/uk/gov/ons/ctp/integration/eqlaunch/service/impl/TestEqLaunchService_payloadCreation.java
+++ src/test/java/uk/gov/ons/ctp/integration/eqlaunch/service/impl/TestEqLaunchService_payloadCreation.java
@@ -2,6 +2,7 @@ package uk.gov.ons.ctp.integration.eqlaunch.service.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Map;
 import java.util.UUID;
 import org.junit.Test;
 import uk.gov.ons.ctp.common.error.CTPException;
@@ -41,7 +42,7 @@ public class TestEqLaunchService_payloadCreation {
 
     // Run code under to test to get a payload. Then replace elements which change on each run with
     // fixed text
-    String payload =
+    Map<String, String> payloadMap =
         eqLaunchService.createPayloadString(
             language,
             channel,
@@ -50,36 +51,37 @@ public class TestEqLaunchService_payloadCreation {
             questionnaireId,
             accountServiceUrl,
             accountServiceLogoutUrl);
-    payload = cleanUUID(payload, "'jti': '", "88888888-8888-8888-8888-888888888888");
-    payload = cleanUUID(payload, "'tx_id': '", "88888888-8888-8888-8888-888888888888");
-    payload = cleanTimestamp(payload, "'iat': ", "12345");
-    payload = cleanTimestamp(payload, "'exp': ", "12345");
+
+    String payload = payloadMap.toString();
+    payload = cleanUUID(payload, "jti=", "88888888-8888-8888-8888-888888888888");
+    payload = cleanUUID(payload, "tx_id=", "88888888-8888-8888-8888-888888888888");
+    payload = cleanTimestamp(payload, "iat=", "12345");
+    payload = cleanTimestamp(payload, "exp=", "12345");
 
     StringBuilder expectedPayload = new StringBuilder();
     expectedPayload.append("{");
-    expectedPayload.append("'jti': '88888888-8888-8888-8888-888888888888', ");
-    expectedPayload.append("'tx_id': '88888888-8888-8888-8888-888888888888', ");
-    expectedPayload.append("'iat': 12345, ");
-    expectedPayload.append("'exp': 12345, ");
-    expectedPayload.append("'case_type': 'H', ");
+    expectedPayload.append("jti=88888888-8888-8888-8888-888888888888, ");
+    expectedPayload.append("tx_id=88888888-8888-8888-8888-888888888888, ");
+    expectedPayload.append("iat=12345, ");
+    expectedPayload.append("exp=12345, ");
+    expectedPayload.append("case_type=H, ");
+    expectedPayload.append("collection_exercise_sid=" + collectionExerciseId.toString() + ", ");
+    expectedPayload.append("region_code=GB-ENG, ");
+    expectedPayload.append("ru_ref=" + uprn + ", ");
+    expectedPayload.append("case_id=" + caseId + ", ");
+    expectedPayload.append("language_code=en, ");
+    expectedPayload.append("display_address=ONS, Segensworth\'s Road, ");
+    expectedPayload.append("response_id=11100000009, ");
+    expectedPayload.append("account_service_url=http://localhost:9092/start, ");
     expectedPayload.append(
-        "'collection_exercise_sid': '" + collectionExerciseId.toString() + "', ");
-    expectedPayload.append("'region_code': 'GB-ENG', ");
-    expectedPayload.append("'ru_ref': '" + uprn + "', ");
-    expectedPayload.append("'case_id': '" + caseId + "', ");
-    expectedPayload.append("'language_code': 'en', ");
-    expectedPayload.append("'display_address': 'ONS, Segensworth\\'s Road', ");
-    expectedPayload.append("'response_id': '11100000009', ");
-    expectedPayload.append("'account_service_url': 'http://localhost:9092/start', ");
-    expectedPayload.append(
-        "'account_service_log_out_url': 'http://localhost:9092/start/save-and-exit', ");
-    expectedPayload.append("'channel': 'field', ");
-    expectedPayload.append("'user_id': '1234567890', ");
-    expectedPayload.append("'questionnaire_id': '11100000009', ");
-    expectedPayload.append("'eq_id': 'census', ");
-    expectedPayload.append("'period_id': '2019', ");
-    expectedPayload.append("'form_type': 'individual_gb_eng', ");
-    expectedPayload.append("'survey': 'CENSUS'");
+        "account_service_log_out_url=http://localhost:9092/start/save-and-exit, ");
+    expectedPayload.append("channel=field, ");
+    expectedPayload.append("user_id=1234567890, ");
+    expectedPayload.append("questionnaire_id=11100000009, ");
+    expectedPayload.append("eq_id=census, ");
+    expectedPayload.append("period_id=2019, ");
+    expectedPayload.append("form_type=individual_gb_eng, ");
+    expectedPayload.append("survey=CENSUS");
     expectedPayload.append("}");
     String expected = expectedPayload.toString();
 
